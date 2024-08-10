ad0cfbf4bb3bd0a1bbea939b72d8404e433ca3e5
DanushH <danu.nherath@gmail.com>
2024-08-10 14:22:20 +0530

    Add requirements

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # NLP_hate_speech_classification
+
+## How to run
+
+```bash
+conda create -n hate python=3.12 -y
+```
+
+```bash
+conda activate hate
+```
+
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate_speech_classification",
+    version="0.0.1",
+    author="Danu",
+    author_email="danu.nherath@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
